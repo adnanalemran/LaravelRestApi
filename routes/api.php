ba6728f6874b04860dba8f832142d930e84ae551
adnanalemran <adnanalemranontor@gmail.com>
2024-06-10 23:27:11 +0600

    chore: Remove unused categoryController file and update API routes

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,3 @@
 
 use App\Http\Controllers\API\categoryController;
 use Illuminate\Support\Facades\Route;
-
-
-Route::get("catagories",  [categoryController::class, 'index']);
-
-Route::post("catagories/store",  [categoryController::class, 'store']);
