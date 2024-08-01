1668197ff6ce4cf9646c562884d85e7981ede5ea
adnanalemran <adnanalemranontor@gmail.com>
2024-08-02 01:49:08 +0600

    chore: Update welcome page to Adnan's Page and add API route

---
+++ resources/views/adnanPage.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Adnan's Page</title>
+</head>
+
+<body>
+    <h1>Welcome to Adnan's Page</h1>
+    <p>This is a sample page created using Laravel's Blade template engine.</p>
+</body>
+
+</html>
